8254cf12c9127cdd8a04d78958e5f12f0bdd1321
GW391 <gw391@wfamily.co.uk>
2020-07-10 16:57:46 +0100

    Setup
    	- Fixed setup procedure
    	- Enabled server and port choices during setup

--- Melessqd/template/setup.php
+++ Melessqd/template/setup.php
@@ -1,8 +1,20 @@
-<?
+<?php
+
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2020 Gregor
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 ?>
@@ -22,6 +34,15 @@ if (isset($_POST['DatabaseType'])){
     $Salt = generateRandomString(20);
     $DataKey = generateRandomString(55);
     $PostKey = generateRandomString(33);
+    $sqltypes = [MySQLi, MySQL];
+    
+    
+    if (in_array($sqltypes, $_POST['DatabaseType'], true)){
+        require 'template/SQL_'.$_POST['DatabaseType'].'.php';
+    }else{
+        // no Database type specified, assume MySQLi
+        require 'template/SQL_MySQLi.php';
+    }
     
     require_once 'template/vars.php';
     $config = "
