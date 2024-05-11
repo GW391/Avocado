59e7f3153b2f6fad9b66f510e094fb40b8e68208
GW391 <67109521+GW391@users.noreply.github.com>
2024-05-11 21:34:46 +0100

    Revert "rename"
    This reverts commit f306c4b227c92a682c9fcb99cb628f4d8804193f.

---
+++ Avocado/template/config.php
@@ -0,0 +1,11 @@
+
+        <?php
+    $DatabaseType = "MySQLi";
+    $DatabaseName = "Tree";
+    $DatabaseServerName = "localhost";$DatabaseServerPort = "3306";
+    $DatabaseUserName = "Tree";
+    $DatabasePassword = "Tree33";
+    $DataKey = "ijRMX80eT4XhjZ1W7gyJDbbFFE9g0DoAojsjNDgXOSOrOJyrTnJ3xgo";
+    $PostKey = "9mddWswWkmZsKUf321ZoTuHcYlL1KYzsM";
+    $Salt = "hSvVk4PJPioULlluMBEb";
+?>    
